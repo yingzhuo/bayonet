df914fb9df591575aa721efa90799b7bc774d009
应卓 <yingzhor@gmail.com>
2026-07-14 13:59:50 +0800

    docs: 为 SpringApplicationGroup/Name/Version 注解补充 Javadoc

---
+++ projects-core/bayonet-boot4-starter-common/src/main/java/com/github/yingzhuo/bayonet/inject/SpringApplicationVersion.java
@@ -0,0 +1,23 @@
+package com.github.yingzhuo.bayonet.inject;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import java.lang.annotation.*;
+
+/**
+ * 注入 {@code spring.application.version} 配置属性的注解。
+ *
+ * <pre>{@code
+ * &#064;SpringApplicationVersion
+ * private String version;
+ * }</pre>
+ *
+ * @see SpringApplicationGroup
+ * @see SpringApplicationName
+ */
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Value("${spring.application.version}")
+public @interface SpringApplicationVersion {
+}
