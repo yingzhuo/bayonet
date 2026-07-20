659484d9196166aff6758172477abb31fef8bd70
应卓 <yingzhor@gmail.com>
2026-07-20 15:14:52 +0800

    fix: 删除不必要的复杂父子类关系

--- projects-core/bayonet-boot4-starter-webcli/src/main/java/com/github/yingzhuo/bayonet/webcli/factory/AbstractClientHttpRequestFactoryBean.java
+++ projects-core/bayonet-boot4-starter-webcli/src/main/java/com/github/yingzhuo/bayonet/webcli/factory/AbstractClientHttpRequestFactoryBean.java
@@ -3,6 +3,7 @@ package com.github.yingzhuo.bayonet.webcli.factory;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.http.client.ClientHttpRequestFactory;
 
+@Deprecated(forRemoval = true)
 public abstract class AbstractClientHttpRequestFactoryBean implements FactoryBean<ClientHttpRequestFactory> {
 
     @Override
