a78b5622e088815029d1b608e9031dbfe51d51e2
应卓 <yingzhor@gmail.com>
2026-07-21 14:02:26 +0800

    refactor: 使用 SSLFactory 重构 JdkClientHttpRequestFactoryBean，新增 UnsafeJdkClientHttpRequestFactoryBean

--- project-integration-test/src/main/java/bayonet/test/ApplicationBoot.java
+++ project-integration-test/src/main/java/bayonet/test/ApplicationBoot.java
@@ -1,42 +1,15 @@
 package bayonet.test;
 
-import com.github.yingzhuo.bayonet.webcli.util.InterceptorFactories;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.web.client.RestClient;
 
 @Slf4j
 @SpringBootApplication
-public class ApplicationBoot implements ApplicationRunner {
-
-    @Autowired
-    @Qualifier("unsafeClientHttpRequestFactory")
-    private ClientHttpRequestFactory clientHttpRequestFactory;
+public class ApplicationBoot {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationBoot.class, args);
     }
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        var cli = RestClient.builder()
-                .requestFactory(clientHttpRequestFactory)
-                .requestInterceptor(InterceptorFactories.createBearerAuthInterceptor("fuck_you"))
-                .baseUrl("https://github.com")
-                .build();
-
-        var html = cli.get()
-                .uri("/yingzhuo/bayonet")
-
-                .retrieve()
-                .requiredBody(String.class);
-
-        log.info("\n{}\n", html);
-    }
 }
--- project-integration-test/src/main/java/bayonet/test/ApplicationBootBean.java
+++ project-integration-test/src/main/java/bayonet/test/ApplicationBootBean.java
@@ -1,19 +1,7 @@
 package bayonet.test;
 
-import com.github.yingzhuo.bayonet.bean.ImportText;
-import com.github.yingzhuo.bayonet.webcli.factory.JdkClientHttpRequestFactoryBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ImportText("classpath:1.txt")
 public class ApplicationBootBean {
-
-    @Bean("unsafeClientHttpRequestFactory")
-    public JdkClientHttpRequestFactoryBean jdkClientHttpRequestFactoryBean() {
-        var bean = new JdkClientHttpRequestFactoryBean();
-        bean.setTrustAllIfNoTrustStore(true);
-        return bean;
-    }
-
 }
