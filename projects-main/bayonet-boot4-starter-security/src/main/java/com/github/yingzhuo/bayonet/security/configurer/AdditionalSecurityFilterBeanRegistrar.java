a63fa8d82347850475c520d91e428942f28fff45
应卓 <yingzhor@gmail.com>
2026-07-30 14:16:40 +0800

    feat: AdditionalSecurityFilter 新增 skipIfAnyProfileActivated 属性

--- projects-main/bayonet-boot4-starter-security/src/main/java/com/github/yingzhuo/bayonet/security/configurer/AdditionalSecurityFilterBeanRegistrar.java
+++ projects-main/bayonet-boot4-starter-security/src/main/java/com/github/yingzhuo/bayonet/security/configurer/AdditionalSecurityFilterBeanRegistrar.java
@@ -10,6 +10,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * {@link AdditionalSecurityFilter @AdditionalSecurityFilter} 注解的 Bean 定义注册器。
  * <p>实现 {@link org.springframework.context.annotation.ImportBeanDefinitionRegistrar ImportBeanDefinitionRegistrar}，
@@ -35,10 +38,20 @@ class AdditionalSecurityFilterBeanRegistrar extends BeanRegistrarSupport {
         );
 
         for (var attributes : attributesSet) {
+            var skipIfAnyProfileActivated = attributes.getStringArray("skipIfAnyProfileActivated");
+
+            // 当任一指定 Profile 激活时，跳过此过滤器
+            if (skipIfAnyProfileActivated.length > 0) {
+                var activeProfiles = List.of(super.environment.getActiveProfiles());
+                var shouldSkip = Arrays.stream(skipIfAnyProfileActivated).anyMatch(activeProfiles::contains);
+                if (shouldSkip) {
+                    continue;
+                }
+            }
+
             var filterType = attributes.<Filter>getClass("filterType");
             var positionFilterType = attributes.<Filter>getClass("positionFilterType");
             var hint = attributes.<FilterPositionHint>getEnum("hint");
-
             var conf = new AdditionalFilterConfig(
                     filterType,
                     positionFilterType,
