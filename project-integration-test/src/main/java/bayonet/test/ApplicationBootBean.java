45b3ef990076ba0f96e58e17eddaffb6aee989dc
应卓 <yingzhor@gmail.com>
2026-08-01 06:05:05 +0800

    refactor: 重组织集成测试密钥库资源到 secret 目录

--- project-integration-test/src/main/java/bayonet/test/ApplicationBootBean.java
+++ project-integration-test/src/main/java/bayonet/test/ApplicationBootBean.java
@@ -3,9 +3,12 @@ package bayonet.test;
 import com.github.yingzhuo.bayonet.jwt.algorithm.SM2Algorithm;
 import com.github.yingzhuo.bayonet.secret.KeyStoreType;
 import com.github.yingzhuo.bayonet.secret.SecretBox;
+import com.github.yingzhuo.bayonet.utility.AES;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+
+import javax.crypto.SecretKey;
 
 @Configuration
 public class ApplicationBootBean {
@@ -13,7 +16,7 @@ public class ApplicationBootBean {
     @Bean
     public SecretBox secretBox() {
         return SecretBox.builder()
-                .resource(new ClassPathResource("config/secret-box.bcfks"))
+                .resource(new ClassPathResource("secret/secret-box.bcfks"))
                 .type(KeyStoreType.BCFKS)
                 .storepass("123456")
                 .build();
@@ -23,6 +26,11 @@ public class ApplicationBootBean {
     public SM2Algorithm hello(SecretBox secretBox) {
         var kp = secretBox.getKeyPair("SM2");
         return new SM2Algorithm(kp.getPublic(), kp.getPrivate());
+    }
+
+    @Bean
+    public AES aes(SecretBox secretBox) {
+        return new AES(secretBox.<SecretKey>getSecretKey("AES"));
     }
 
 }
