6c6f19df52d2d73e86110eda567b86dc15a19d48
应卓 <yingzhor@gmail.com>
2026-08-05 05:33:16 +0800

    feat: 新增 Named/Identified 接口及 RichUserDetails

--- projects-main/bayonet-boot4-starter-common/src/main/java/com/github/yingzhuo/bayonet/common/TODO.java
+++ projects-main/bayonet-boot4-starter-common/src/main/java/com/github/yingzhuo/bayonet/common/TODO.java
@@ -3,7 +3,7 @@ package com.github.yingzhuo.bayonet.common;
 import org.jetbrains.annotations.ApiStatus;
 
 /**
- * 暂未实现
+ * 占位用暂未实现异常
  * <p>主要让 AI Agent识别，不作其他用途。</p>
  *
  * @author 应卓
---
+++ projects-main/bayonet-boot4-starter-security/src/main/java/com/github/yingzhuo/bayonet/security/authentication/RichUserDetailsImpl.java
@@ -0,0 +1,107 @@
+package com.github.yingzhuo.bayonet.security.authentication;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * {@link RichUserDetails} 的不可变默认实现。
+ *
+ * @author 应卓
+ * @see RichUserDetails
+ * @since 4.1.1
+ */
+final class RichUserDetailsImpl implements RichUserDetails {
+
+    private final @Nullable String id;
+    private final @Nullable LocalDate dob;
+    private final @Nullable String email;
+    private final String username;
+    private final @Nullable String password;
+    private final Set<GrantedAuthority> authorities;
+    private final boolean enabled;
+    private final boolean accountNonExpired;
+    private final boolean credentialsNonExpired;
+    private final boolean accountNonLocked;
+
+    RichUserDetailsImpl(
+            @Nullable String id,
+            @Nullable LocalDate dob,
+            @Nullable String email,
+            String username,
+            @Nullable String password,
+            Collection<? extends GrantedAuthority> authorities,
+            boolean enabled,
+            boolean accountNonExpired,
+            boolean credentialsNonExpired,
+            boolean accountNonLocked) {
+        this.id = id;
+        this.dob = dob;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.authorities = Set.copyOf(authorities);
+        this.enabled = enabled;
+        this.accountNonExpired = accountNonExpired;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    @Override
+    public @Nullable String getId() {
+        return id;
+    }
+
+    @Override
+    public @Nullable LocalDate getDob() {
+        return dob;
+    }
+
+    @Override
+    public @Nullable String getEmail() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public String toString() {
+        return "RichUserDetailsImpl{username='" + username + "', enabled=" + enabled + '}';
+    }
+}
